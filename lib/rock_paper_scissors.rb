a12eb3f3f7b3b38ff0489754a5d0b0251b9d0ffa
Aaron Anderson <aaron.anderson.a7x@gmail.com>
2017-06-05 21:29:11 +0000

    Part 3 complete

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,14 +1,23 @@
 class RockPaperScissors
 
-  # Exceptions this class can raise:
-  class NoSuchStrategyError < StandardError ; end
+  	# Exceptions this class can raise:
+  	class NoSuchStrategyError < StandardError ; end
 
-  def self.winner(player1, player2)
-    # YOUR CODE HERE
-  end
+  	def self.winner(player1, player2)
+		case player1[1] + player2[1]
+		when "SP", "RS", "PR" then player1
+		when "PS", "SR", "RP" then player2
+		when 'PP', 'SS', 'RR' then player1
+		else raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+		end
+  	end
 
-  def self.tournament_winner(tournament)
-    # YOUR CODE HERE
-  end
+  	def self.tournament_winner(tournament)
+    		if tournament[0][0].kind_of?(String)
+			winner(tournament[0], tournament[1])
+		else
+			winner(tournament_winner(tournament[0]), tournament_winner(tournament[1]))
+		end
+  	end
 
 end
